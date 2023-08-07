b82a46fe33e3d1b0b8b18c7831bea8d9eb63c92d
Marcela Alonso <MARCELA.ALONSO.S@GMAIL.COM>
2023-08-07 12:10:30 -0400

    post question

--- quizapp/src/main/java/com/telusko/quizapp/controller/QuestionController.java
+++ quizapp/src/main/java/com/telusko/quizapp/controller/QuestionController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +27,10 @@ public class QuestionController {
   @GetMapping("category/{category}")
   public List<Question> getQuestionsByCategory(@PathVariable String category) {
     return questionService.getQuestionsByCategory(category);
+  }
+
+  @PostMapping("add")
+  public String addQuestion(@RequestBody Question question) {
+    return questionService.addQuestion(question);
   }
 }
